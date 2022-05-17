401d5399a842ad8917c69f145f09376d94a64b7e
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-17 19:33:44 +0530

    Add project files.

---
+++ Method Switch case cal Return/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Switch_case_cal_Return
+{
+    class Program
+    {
+        static double prime(double num, double num2, char choice)
+        {
+            double res = 0;
+            switch (choice)
+            {
+                case '+':
+                    res = num + num2;
+                    break;
+                case '-':
+                    res = num - num2;
+                    break;
+                case '*':
+                    res = num * num2;
+                    break;
+                case '/':
+                    res = num / num2;
+                    break;
+                default:
+                    Console.WriteLine("Not Found Result :");
+                    break;
+
+            }
+            return res;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the number :");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the choice:");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            double show = prime(a, b, c);
+            Console.WriteLine("Result Is :" + show);
+            Console.ReadLine();
+        }
+    }
+}
